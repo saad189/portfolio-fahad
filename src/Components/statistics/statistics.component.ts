94cc8ce15cb8389cfd1469813816e61dca701154
Saad Ahmed <saad18910@hotmail.com>
2022-01-03 00:59:10 +0500

    Help

--- src/Components/statistics/statistics.component.ts
+++ src/Components/statistics/statistics.component.ts
@@ -12,8 +12,38 @@ export class StatisticsComponent implements OnInit {
       title: "Our Team",
       content: `I have a great team of people with me, Each an Expert in their domain.
     Our Vision and ideas complement each other; Leading to a great product for our clients`,
-      width: "10rem",
-      height: "10rem"
+      width: "30rem",
+      height: "25rem"
+    },
+    {
+      title: '12+',
+      content: 'Satisfied Clients',
+      width: '12rem',
+      height: '10rem'
+    },
+    {
+      title: '3 Years',
+      content: 'Experience',
+      width: '12rem',
+      height: '10rem'
+    },
+    {
+      title: '100%',
+      content: 'Dedication',
+      width: '12rem',
+      height: '10rem'
+    },
+    {
+      title: '21',
+      content: 'Built Projects',
+      width: '12rem',
+      height: '10rem'
+    },
+    {
+      title: 'Stunning Designs',
+      content: 'We build beautifully designed web and mobile projects for you using modern tools',
+      width: '54rem',
+      height: '25rem'
     }
   ]
   constructor() { }
