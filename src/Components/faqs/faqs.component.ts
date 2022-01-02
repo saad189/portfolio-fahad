a7fb523020cf7ab1ee6862fe887c737b8b2a065e
saadAhmed <saad.ahmed@empglabs.com>
2022-01-02 18:33:06 +0500

    Created basic FAQS

--- src/Components/faqs/faqs.component.ts
+++ src/Components/faqs/faqs.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-faqs',
-  templateUrl: './faqs.component.html',
-  styleUrls: ['./faqs.component.scss']
+  selector: "app-faqs",
+  templateUrl: "./faqs.component.html",
+  styleUrls: ["./faqs.component.scss"],
 })
 export class FaqsComponent implements OnInit {
+  panelOpenState = false;
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  constructor() {}
+  public InfoPair: { Question: string; Answer: string }[] = [
+    {
+      Question: "What Servies do you offer exactly?",
+      Answer: "My name is Fahad and I am a dumbass",
+    },
+    {
+      Question: "How much does a project cost?",
+      Answer: "My name is Fahad and I am a dumbass",
+    },
+    {
+      Question: "Which companies have you worked with?",
+      Answer: "My name is Fahad and I am a dumbass",
+    },
+    {
+      Question: "Do you work internationally?",
+      Answer: "My name is Fahad and I am a dumbass",
+    },
+    {
+      Question: "Payment Terms for a Project?",
+      Answer: "My name is Fahad and I am a dumbass",
+    },
+    {
+      Question: "How long does a project take?",
+      Answer: "My name is Fahad and I am a dumbass",
+    },
+  ];
+  ngOnInit() {}
 }
