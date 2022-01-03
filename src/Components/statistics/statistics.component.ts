28185ed51ffea3bec24c45f2cc0e62be9e0df088
Saad Ahmed <saad18910@hotmail.com>
2022-01-03 22:40:36 +0500

    Oh well, Benefits Structure set up with bootstrap

--- src/Components/statistics/statistics.component.ts
+++ src/Components/statistics/statistics.component.ts
@@ -7,43 +7,51 @@ import { CardInfo } from '../Shared/card-model';
   styleUrls: ['./statistics.component.scss']
 })
 export class StatisticsComponent implements OnInit {
+  width_small = "16rem";
+  height_small = "13rem";
+
+
+  smallCards: CardInfo[] = [
+    {
+      title: '12+',
+      content: 'Satisfied Clients',
+      width: this.width_small,
+      height: this.height_small
+    },
+    {
+      title: '3 Years',
+      content: 'Experience',
+      width: this.width_small,
+      height: this.height_small
+    },
+    {
+      title: '100%',
+      content: 'Dedication',
+      width: this.width_small,
+      height: this.height_small
+    },
+    {
+      title: '21',
+      content: 'Built Projects',
+      width: this.width_small,
+      height: this.height_small
+    }
+  ];
+
   benefitsCards: CardInfo[] = [
     {
       title: "Our Team",
       content: `I have a great team of people with me, Each an Expert in their domain.
     Our Vision and ideas complement each other; Leading to a great product for our clients`,
-      width: "30rem",
-      height: "25rem"
+      width: "33.5rem",
+      height: "26.5rem"
     },
-    {
-      title: '12+',
-      content: 'Satisfied Clients',
-      width: '12rem',
-      height: '10rem'
-    },
-    {
-      title: '3 Years',
-      content: 'Experience',
-      width: '12rem',
-      height: '10rem'
-    },
-    {
-      title: '100%',
-      content: 'Dedication',
-      width: '12rem',
-      height: '10rem'
-    },
-    {
-      title: '21',
-      content: 'Built Projects',
-      width: '12rem',
-      height: '10rem'
-    },
+
     {
       title: 'Stunning Designs',
       content: 'We build beautifully designed web and mobile projects for you using modern tools',
-      width: '54rem',
-      height: '25rem'
+      width: '68rem',
+      height: '24rem'
     }
   ]
   constructor() { }
