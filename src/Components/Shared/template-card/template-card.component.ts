278ee6d7e423f6676fe8f8bf411d7b54979ed07f
saadAhmed <saad.ahmed@empglabs.com>
2022-01-05 00:35:14 +0500

    Basic Template Established

--- src/Components/Shared/template-card/template-card.component.ts
+++ src/Components/Shared/template-card/template-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-template-card',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateCardComponent implements OnInit {
 
+  @Input() model : any;
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
--- src/Components/certifications-info/certifications-info.component.ts
+++ src/Components/certifications-info/certifications-info.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from "@angular/core";
 
 @Component({
-  selector: 'app-certifications-info',
-  templateUrl: './certifications-info.component.html',
-  styleUrls: ['./certifications-info.component.scss']
+  selector: "app-certifications-info",
+  templateUrl: "./certifications-info.component.html",
+  styleUrls: ["./certifications-info.component.scss"],
 })
 export class CertificationsInfoComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  @ViewChild("myTemplate", { static: true }) myTemplate: TemplateRef<any>;
+
+  myModel = {};
 
   ngOnInit() {
+    this.myModel = {
+      template: this.myTemplate,
+    };
   }
-
 }
