746f6ce028488535283651aa3d39a81a488d5197
saadAhmed <saad.ahmed@empglabs.com>
2022-01-05 01:05:57 +0500

    Beautiful

--- src/Components/certifications-info/certifications-info.component.ts
+++ src/Components/certifications-info/certifications-info.component.ts
@@ -10,11 +10,60 @@ export class CertificationsInfoComponent implements OnInit {
 
   @ViewChild("myTemplate", { static: true }) myTemplate: TemplateRef<any>;
 
+  @ViewChild("researched", { static: true }) researched: TemplateRef<any>;
+
+  @ViewChild("designtools", { static: true }) designtools: TemplateRef<any>;
+
+  @ViewChild("certifications", { static: true })
+  certifications: TemplateRef<any>;
+
+  @ViewChild("responsive", { static: true }) responsive: TemplateRef<any>;
+
+  refUrl = "../../assets";
+
+  designImages: string[] = [
+    `${this.refUrl}/instagram.webp`,
+    `${this.refUrl}/wordpress.png`,
+    `${this.refUrl}/facebook.png`,
+    `${this.refUrl}/XD.png`,
+    `${this.refUrl}/photoshop.png`,
+    `${this.refUrl}/illustrator.png`,
+  ];
+
+  researchedUrl = `${this.refUrl}/`;
+  responsiveUrl = `${this.refUrl}/responsive.png`;
+  udemyUrl = `${this.refUrl}/udemy.png`;
+
   myModel = {};
+
+  researchedModel = {};
+
+  designtoolsModel = {};
+
+  certificationsModel = {};
+
+  responsiveModel = {};
 
   ngOnInit() {
     this.myModel = {
       template: this.myTemplate,
+      subheading: "this is my subheading",
+    };
+
+    this.researchedModel = {
+      template: this.researched,
+    };
+
+    this.designtoolsModel = {
+      template: this.designtools,
+    };
+
+    this.certificationsModel = {
+      template: this.certifications,
+    };
+
+    this.responsiveModel = {
+      template: this.responsive,
     };
   }
 }
