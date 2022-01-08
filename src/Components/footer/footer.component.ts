76931288339164c6151d759d1f99b375179c7b67
Saad Ahmed <saad18910@hotmail.com>
2022-01-09 01:24:55 +0500

    Footer Fixed

--- src/Components/footer/footer.component.ts
+++ src/Components/footer/footer.component.ts
@@ -8,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class FooterComponent implements OnInit {
 
   constructor() { }
-
+  year = '2022';
+  name = 'Muhammad Fahad Nasir';
   ngOnInit() {
   }
 
--- src/Components/process/process.component.ts
+++ src/Components/process/process.component.ts
@@ -12,16 +12,16 @@ export class ProcessComponent implements OnInit {
       subtitle: `Step 1`,
       title: `Project Request`,
       content: `At the beginning of our collaboration is the project request. Your Information
-      helps me understand if I am the right person for your project. If yes, we will arrange a video call so I can learn more.`,
+      helps me to understand if I am the right person for your project. If yes, we will arrange a video call so I can learn more.`,
     },
     {
       subtitle: `Step 2`,
       title: `Project Request`,
       content: `At the beginning of our collaboration is the project request. Your Information
-      helps me understand if I am the right person for your project. If yes, we will arrange a video call so I can learn more.`,
+      helps me to understand if I am the right person for your project. If yes, we will arrange a video call so I can learn more.`,
     },
   ];
-  constructor() {}
+  constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() { }
 }
